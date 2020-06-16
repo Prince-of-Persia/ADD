277878330c4ebe260eefeb6c0c57cb6f3087ef3f
Amir <amir.rajabifar@gmail.com>
2020-06-16 18:24:19 +0800

    LED progress bar

--- final_code/FINAL/FINAL.ino
+++ final_code/FINAL/FINAL.ino
@@ -7,7 +7,7 @@
 #define dcmotor 2
 #define limits A8
 //LEDs
-int ledPin[] = {22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41};
+int ledPin[] = {22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53};
 
 
 #include <Servo.h>
@@ -71,6 +71,7 @@ void loop()
   //Serial.println(fsrreading2);
   if (seq == 0) // First FSR (start of CMP)
   {
+  	digitalWrite(ledPin[0], HIGH);
     fsrreading1 = analogRead(fsr1pin);
     if (fsrreading1 > 150)
     {
@@ -85,34 +86,27 @@ void loop()
   
   if (seq == 1) // Opening first servo gate
   {
+    digitalWrite(ledPin[1], HIGH); 
     for(angle = 10; angle < 90; angle++)  
     {                                  
-      servo.write(angle); 
-      digitalWrite(ledPin[0], HIGH);              
-      delay(delayTime); 
-      digitalWrite(ledPin[0], LOW);   
-    	digitalWrite(ledPin[0], HIGH);              
-      delay(delayTime); 
-      digitalWrite(ledPin[0], LOW);               
+      servo.write(angle);              
+      delay(delayTime);              
     }
     seq++;
   }
   
   if (seq == 2) // Closing first servo gate
   {
+    digitalWrite(ledPin[2], HIGH); 
     angle = 10;
-    servo.write(angle); 
-    digitalWrite(ledPin[1], HIGH);              
+    servo.write(angle);           
     delay(delayTime); 
-    digitalWrite(ledPin[1], LOW);   
-  	digitalWrite(ledPin[1], HIGH);              
-    delay(delayTime); 
-    digitalWrite(ledPin[1], LOW);
     seq++;
   }
   
   if (seq == 3) // Second FSR (end of ramp)
   {
+    digitalWrite(ledPin[3], HIGH); 
     fsrreading2 = analogRead(fsr2pin);
     if (fsrreading2 > 100)
     {
@@ -121,15 +115,8 @@ void loop()
         if (fsrreading2 > 100) 
         {
           digitalWrite(crane, LOW);
-					digitalWrite(ledPin[2], HIGH);              
-					delay(delayTime); 
-					digitalWrite(ledPin[2], LOW);
-          delay(delayTime);
-          delay(60);
-          digitalWrite(crane, HIGH);
-					digitalWrite(ledPin[2], HIGH);              
-					delay(delayTime); 
-					digitalWrite(ledPin[2], LOW);          
+          delay(100);
+          digitalWrite(crane, HIGH);        
           seq++;
         }
     }
@@ -137,6 +124,7 @@ void loop()
   
   if (seq == 4) // First FSR (start of CMP)
   {
+    digitalWrite(ledPin[4], HIGH);  	
     fsrreading3 = analogRead(fsr3pin);
     if (fsrreading3 > 100)
     {
@@ -146,6 +134,7 @@ void loop()
   
   if (seq == 5) // Opening first servo gate
   {
+    digitalWrite(ledPin[5], HIGH);  	
     delay(3000);
     servo2.write(30);
     seq++;        
@@ -153,6 +142,7 @@ void loop()
   
   if (seq == 6) // Closing first servo gate
   {
+    digitalWrite(ledPin[6], HIGH);  	
     delay(1000);
     servo2.write(95); 
     seq++;
@@ -160,6 +150,7 @@ void loop()
   
   if (seq == 7) // Truck FSR (start of CMP)
   {
+  	digitalWrite(ledPin[7], HIGH);
     fsrreading4 = analogRead(fsr4pin);
     if (fsrreading4 > 150)
     {
@@ -174,6 +165,7 @@ void loop()
   
   if (seq == 8)  // Truck move
   {
+  	digitalWrite(ledPin[8], HIGH);
     analogWrite(dcmotor, 100);
     
     if (digitalRead(limits) == LOW)
@@ -185,13 +177,10 @@ void loop()
   
   if (seq == 9) // Tell crane to pick up
   {
+  	digitalWrite(ledPin[9], HIGH);
     digitalWrite(crane, LOW);
     delay(1000);  
     digitalWrite(crane, HIGH);
     seq++; 
   } 
-  
-  
-  
-  
 }
